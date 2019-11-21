a751d78abb29e947e47382c9e6e70b2471e40f9d
Zakaria ElQotbi <zakaria@ghs.com>
2019-11-21 22:48:47 +0100

    experimentation/compo: set base for benchmarking

--- video-scripts/experimentation/compo-gst1.0-4surfaces.sh
+++ video-scripts/experimentation/compo-gst1.0-4surfaces.sh
@@ -20,6 +20,9 @@
 # To play the resulted video:
 # $ gst-launch-1.0 filesrc location=out.mp4 ! decodebin ! videoconvert! autovideosink
 
+#framerate=30
+framerate=25
+
 gsink_z=0
 gsink_h=1080
 gsink_w=1920
@@ -31,11 +34,19 @@ sink_w=$[gsink_w/2]
 #OUTPUT="videoconvert ! autovideosink"
 #OUTPUT="jpegenc ! avimux ! filesink location=out.avi"
 #OUTPUT="x264enc pass=quant ! matroskamux ! filesink location=out.mkv"
-OUTPUT="x264enc pass=5 quantizer=25 speed-preset=6 ! video/x-h264, profile=baseline ! mp4mux ! filesink location=out.mp4"
+#OUTPUT="x264enc pass=5 quantizer=25 speed-preset=6 ! video/x-h264, profile=baseline ! mp4mux ! filesink location=out.mp4"
+#OUTPUT="fakesink sync=false"
+OUTPUT="fakevideosink"
 
-gst-launch-1.0 -e videomixer name=mix sink_0::zorder=$sink_z sink_1::zorder=$sink_z sink_2::zorder=$sink_z sink_3::zorder=$sink_z sink_4::zorder=$gsink_z ! \
+#LIMITE_BUFFER=""
+LIMITE_BUFFER="num-buffers=1000"
+
+#PROFILER=""
+PROFILER=gst-top-1.0
+
+$PROFILER gst-launch-1.0 -e videomixer name=mix sink_0::zorder=$sink_z sink_1::zorder=$sink_z sink_2::zorder=$sink_z sink_3::zorder=$sink_z sink_4::zorder=$gsink_z ! \
     $OUTPUT \
-    videotestsrc pattern=1  ! video/x-raw,framerate=30/1,width=${sink_w},height=${sink_h} ! videobox border-alpha=0 top=0          left=0          ! mix. \
-    videotestsrc pattern=15 ! video/x-raw,framerate=30/1,width=${sink_w},height=${sink_h} ! videobox border-alpha=0 top=0          left=-${sink_w} ! mix. \
-    videotestsrc pattern=13 ! video/x-raw,framerate=30/1,width=${sink_w},height=${sink_h} ! videobox border-alpha=0 top=-${sink_h} left=0          ! mix. \
-    videotestsrc pattern=0  ! video/x-raw,framerate=30/1,width=${sink_w},height=${sink_h} ! videobox border-alpha=0 top=-${sink_h} left=-${sink_w} ! mix.
+    videotestsrc $LIMITE_BUFFER pattern=1  ! video/x-raw,framerate=${framerate}/1,width=${sink_w},height=${sink_h} ! videobox border-alpha=0 top=0          left=0          ! mix. \
+    videotestsrc $LIMITE_BUFFER pattern=15 ! video/x-raw,framerate=${framerate}/1,width=${sink_w},height=${sink_h} ! videobox border-alpha=0 top=0          left=-${sink_w} ! mix. \
+    videotestsrc $LIMITE_BUFFER pattern=13 ! video/x-raw,framerate=${framerate}/1,width=${sink_w},height=${sink_h} ! videobox border-alpha=0 top=-${sink_h} left=0          ! mix. \
+    videotestsrc $LIMITE_BUFFER  pattern=0 ! video/x-raw,framerate=${framerate}/1,width=${sink_w},height=${sink_h} ! videobox border-alpha=0 top=-${sink_h} left=-${sink_w} ! mix.
